b48e21ce3f47aa76079a599944c78b9bd60a6003
Carliink <carliianoide@hotmail.com>
2018-02-20 16:01:54 -0600

    Añadido package.json

--- mqttBroker.js
+++ mqttBroker.js
@@ -1,7 +1,21 @@
 var mosca = require('mosca');
+// import * as firebase from 'firebase/app' ;
+// import 'firebase/auth';
+// import 'firebase/database';
+
+// const config = {
+// 	apiKey: "AIzaSyASrcvPNyhSlUYTbg0o2g_MFfw5rTdgnS0",
+//     authDomain: "yinn-7dbd2.firebaseapp.com",
+//     databaseURL: "https://yinn-7dbd2.firebaseio.com",
+//     projectId: "yinn-7dbd2",
+//     storageBucket: "yinn-7dbd2.appspot.com",
+//     messagingSenderId: "17822749213"
+// };
+
 
 var server = new mosca.Server({
-  host: '192.168.1.76',
+  //host: '192.168.1.76',
+  host: '127.0.0.1',
   port: 3000
 });
 
@@ -28,6 +42,9 @@ server.on('unsubscribed', function(topic, client) {
 server.on('ready', function() {
   console.log('Mosca server is up and running');
 });
+// server.on('temperatura', function(topic, client) {
+//   firebase.database().ref('dispositivos/cliente-1').child('temperatura').set(topic.payload)
+// });
 
 var ledCommand = '001';
 
@@ -35,3 +52,12 @@ setInterval(function() {
   ledCommand = (ledCommand === '001') ? '002' : '001';
   server.publish({topic: 'LEDToggle', payload: ledCommand});
 }, 1000);
+
+
+
+// setInterval(function() {
+// 	firebase.database().ref('dispositivos/cliente-1').child('ventilador').on('value',(snapshot)=>{
+// 		ledCommand = snapshot.val();
+// 		server.publish({topic: 'LEDToggle', payload: ledCommand});
+// 	})
+// }, 3000);
