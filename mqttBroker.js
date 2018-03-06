c3b728ea70519f77c5db56368d30341d819cc4e1
Carliink <carliianoide@hotmail.com>
2018-03-06 17:22:49 -0600

    Cambios

--- mqttBroker.js
+++ mqttBroker.js
@@ -15,6 +15,7 @@ var firebase = require("firebase");
 // };
 // Initialize Firebase
 // TODO: Replace with your project's customized code snippet
+
 var config = {
   apiKey: "AIzaSyASrcvPNyhSlUYTbg0o2g_MFfw5rTdgnS0",
   authDomain: "yinn-7dbd2.firebaseapp.com",
@@ -25,12 +26,8 @@ firebase.initializeApp(config);
 
 var server = new mosca.Server({
   //host: '192.168.1.76',
-<<<<<<< Updated upstream
-  host: '192.168.100.13',
-=======
-  host: '192.168.100.151',
->>>>>>> Stashed changes
-  port: 8080
+  host: '100.87.207.100',
+  port: 1883
 });
 
 server.on('clientConnected', function(client) {
@@ -41,9 +38,9 @@ server.on('clientDisconnected', function(client) {
   console.log('client disconnected', client.id);
 });
 
-server.on('published', function(packet, client) {
-  console.log(packet);
-});
+// server.on('published', function(packet, client) {
+//   console.log(packet);
+// });
 
 server.on('subscribed', function(topic, client) {
   console.log('subscribed: ' + client.id);
@@ -53,24 +50,65 @@ server.on('unsubscribed', function(topic, client) {
   console.log('unsubscribed: ' + client.id);    
 });
 
-server.on('message', function(topic, message) {
-  console.log(topic);
-  console.log(message);
+server.on('published', function(packet, client) {
+  let topico = packet.topic;
+  let mensaje = stringToBoolean(packet.payload.toString());
+
+  let obj = {
+    'ventilador': mensaje
+  };
+
+  firebase.database().ref('dispositivos/cliente-1').child('actuadores').update(obj,(function (err) {
+
+  }));
+
+  console.log('Publicado en topic:' + topico + ' - mensaje:' + mensaje);
 });
+
+// server.on('message', function(topic, message) {
+//   //console.log(topic);
+//   //console.log(message);
+
+
+//   let obj = {
+//       'ventilador': message
+//   };
+
+//   firebase.database().ref('dispositivos/cliente-1').child('actuadores').update(obj,(function (err) {
+
+//   }));
+// });
 
 // server.on('temperatura', function(topic, client) {
 //   firebase.database().ref('dispositivos/cliente-1').child('temperatura').set(topic.payload)
 // });
-// var ledCommand = '001';
+var ledCommand = '';
+
 server.on('ready', setup);
 
 // fired when the mqtt server is ready
 function setup() {
   console.log('Mosca está corriendo');
+
+  firebase.database().ref('dispositivos/cliente-1').child('actuadores/ventilador').on('value',(snapshot)=>{
+    ledCommand = snapshot.val();
+    server.publish({topic: 'LEDToggle', payload: ledCommand}, function() {
+      console.log('Dato Actualizado');
+    });
+  })
   // server.subscribe('Conexion');
 }
 
-// var ledCommand = '001';
+function stringToBoolean(string){
+    switch(string.toLowerCase().trim()){
+        case "true": case "yes": case "1": return true;
+        case "false": case "no": case "0": case null: return false;
+        default: return Boolean(string);
+    }
+}
+
+
+//var ledCommand = '001';
 
 // setInterval(function() {
 //   ledCommand = (ledCommand === '001') ? '002' : '001';
@@ -79,8 +117,5 @@ function setup() {
 
 
 // setInterval(function() {
-// 	firebase.database().ref('dispositivos/cliente-1').child('ventilador').on('value',(snapshot)=>{
-// 		ledCommand = snapshot.val();
-// 		server.publish({topic: 'LEDToggle', payload: ledCommand});
-// 	})
+	
 // }, 3000);
